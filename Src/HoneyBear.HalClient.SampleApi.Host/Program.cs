ac12e2d62f0e006c2bb1836d7ae6b80b642d4da3
Eoin O'Connor <eoin55@gmail.com>
2017-04-02 16:21:01 +1000

    [NoTicket] - Added a sample ASP.NET Core API, which will be used for integration tests.

---
+++ Src/HoneyBear.HalClient.SampleApi.Host/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+namespace HoneyBear.HalClient.SampleApi.Host
+{
+    internal sealed class Program
+    {
+        public static void Main(string[] args) =>
+            new WebHostBuilder()
+                .UseKestrel()
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build()
+                .Run();
+    }
+}
---
+++ Src/HoneyBear.HalClient.SampleApi.Host/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace HoneyBear.HalClient.SampleApi.Host
+{
+    internal sealed class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+            => services.AddMvc();
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+            => app.UseMvc();
+    }
+}
